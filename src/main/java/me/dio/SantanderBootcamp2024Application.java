14491cfdc3d53367f7b03c30b6caeb6a1443ce00
jefersonpereira <jeferson_sbc@yahoo.com.br>
2024-07-05 20:37:43 -0300

    primeira revisão

--- src/main/java/me/dio/SantanderBootcamp2024Application.java
+++ src/main/java/me/dio/SantanderBootcamp2024Application.java
@@ -3,9 +3,12 @@ package me.dio;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
+
+@OpenAPIDefinition(servers = {@Server(url = "/", description = "Default Server URL")})
 @SpringBootApplication
 public class SantanderBootcamp2024Application {
-
 	public static void main(String[] args) {
 		SpringApplication.run(SantanderBootcamp2024Application.class, args);
 	}
